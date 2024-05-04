845ec95eb598068af275a29959791140f4aa5163
rahulry26 <ry33420@gmail.com>
2024-05-04 10:02:49 +0530

    ecommerce

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+import Herosection from './components/Hero';
+import Navigation from './components/Navigation';
+
+const App= () =>{
+    return (
+    <diV>
+          
+
+
+<Navigation/>
+<Herosection/>
+
+    </diV>);
+};
+
+export default App;
